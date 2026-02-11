ecdb7f42e946c88b2cee165b5567d8a68d37f10c
WajahatAli8710 <wajahatali8710@gmail.com>
2026-02-11 20:08:43 +0530

    change port logic

--- day-09/Backend/server.js
+++ day-09/Backend/server.js
@@ -4,6 +4,7 @@ const connectToDB = require("./src/config/database")
 
 connectToDB()
 
-app.listen("3000", () => {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
   console.log("Server is listen on port 3000");
 });
