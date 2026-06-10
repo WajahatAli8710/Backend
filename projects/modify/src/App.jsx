1ab21ef06e4f29796fc0a0ce5fdafc0689ac768c
WajahatAli8710 <wajahatali8710@gmail.com>
2026-06-11 02:10:25 +0530

    start moodify project

---
+++ projects/modify/src/App.jsx
@@ -0,0 +1,11 @@
+import FaceExpression from "./features/expression/components/FaceExpression";
+
+function App() {
+  return (
+    <div>
+      <FaceExpression />
+    </div>
+  );
+}
+
+export default App;
